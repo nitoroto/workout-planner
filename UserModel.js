6fcf4c1cefe6281fd99f5ad1aeee8490bf0e1206
nitoroto <161154942+nitoroto@users.noreply.github.com>
2024-05-09 17:59:19 +0200

    new update

--- UserModel.js
+++ UserModel.js
@@ -6,7 +6,7 @@ const UserSchema = new mongoose.Schema({
     ref: 'Workout',
     index: true,
   }],
-});
+}, { timestamps: true });
 
 const UserModel = mongoose.model('User', UserSchema);
 
@@ -15,15 +15,51 @@ async function linkWorkoutToUser(userId, workoutId) {
     const user = await UserModel.findById(userId);
     if (!user) throw new Error('User not found');
     
-    user.workoutList.push(workoutId);
-    await user.save();
-    console.log('Workout added successfully to user.');
+    if (!user.workoutList.includes(workoutId)) {
+      user.workoutList.push(workoutId);
+      await user.save();
+      console.log('Workout added successfully to user.');
+    } else {
+      console.log('Workout already exists in user\'s list.');
+    }
   } catch (error) {
     console.error('Error while linking workout to user:', error.message);
+  }
+}
+
+async function unlinkWorkoutFromUser(userId, workoutId) {
+  try {
+    const user = await UserModel.findById(userId);
+    if (!user) throw new Error('User not found');
+
+    const index = user.workoutList.indexOf(workoutId);
+    if (index > -1) {
+      user.workoutList.splice(index, 1);
+      await user.save();
+      console.log('Workout removed successfully from user.');
+    } else {
+      console.log('Workout not found in user\'s list.');
+    }
+  } catch (error) {
+    console.error('Error while unlinking workout from user:', error.message);
+  }
+}
+
+async function getUserWorkouts(userId) {
+  try {
+    const userWorkouts = await UserModel.findById(userId).populate('workoutList').exec();
+    if (!userWorkouts) throw new Error('User not found');
+    
+    console.log('User workouts: ', userWorkouts.workoutList);
+    return userWorkouts.workoutList;
+  } catch (error) {
+    console.error('Error while fetching user workouts:', error.message);
   }
 }
 
 module.exports = {
   UserModel,
   linkWorkoutToUser,
+  unlinkWorkoutFromUser,
+  getUserWorkouts,
 };
